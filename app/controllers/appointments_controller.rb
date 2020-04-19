309bc3e09ae66b97001df29563733081bf7f7442
anikachristopher <anika.mchristoper@gmail.com>
2020-04-19 03:16:05 -0400

    create methods in appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,34 @@
 class AppointmentsController < ApplicationController
+    before_action :login_required
+
+    def new
+        @appointment = Appointment.new
+    end
+
+    def create
+        @appointment = current_client.appointments.build(appointment_params(:date, :time))
+        if appointment.save
+            redirect_to appointment_path(@appointment)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    def destroy
+    end
+
+    private
+
+    def appointment_params
+    end
+    
 end
