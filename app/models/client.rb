e014d2cd00e3c143d3dc9d2a21816a0efa8bce04
anikachristopher <anika.mchristoper@gmail.com>
2020-04-18 20:31:51 -0400

    create session controller and routes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,6 @@
 class Client < ApplicationRecord
     has_secure_password
+
+    validates :first_name, :last_name, :email, presence :true
+    validates :first_name, :last_name, :email, uniqueness :true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup' => 'clients#new'
+  get '/login' => 'sessions#new'
+  
   resources :clients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
